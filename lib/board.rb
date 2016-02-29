be27bb0df445b229f3c3d1b2894770914bfe8383
Shmuwol <shmullywolfson@gmail.com>
2016-02-28 20:45:13 -0500

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,47 @@
+class Board
+  attr_accessor :cells
+
+  def initialize
+    self.reset!
+  end
+
+  def reset!
+    self.cells = Array.new(9," ")
+  end
+
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+  def position(cell)
+    self.cells[cell.to_i-1]
+  end
+
+  def full?
+    cells.all? { |cell| cell != " "}
+  end
+
+  def turn_count
+    cells.count { |cell| cell != " " }
+  end
+
+  def taken?(cell)
+    self.position(cell) != " "
+  end
+
+  def valid_move?(move)
+    !self.taken?(move) && move.to_i.between?(1,9)
+  end
+
+  def update(position, player)
+
+  end
+
+
+
+
+end
